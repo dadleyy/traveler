48943b20c4a9e7f407bfc894d4617030d7ee98b8
Danny Hadley <dadleyy@gmail.com>
2021-03-12 17:18:14 -0500

    add caching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+pub use std::collections::HashMap;
+
+const MOD: i32 = (10 ^ 9) + 7;
+
 fn main() {
   assert_eq!(count_routes(vec![2, 3, 6, 8, 4], 1, 3, 5), 4);
 }
@@ -11,7 +15,11 @@ struct Movement {
   paths: i32,
 }
 
-fn inner_count(locations: &Vec<i32>, mut state: Movement) -> Movement {
+fn inner_count(
+  locations: &Vec<i32>,
+  mut state: Movement,
+  cache: &mut HashMap<usize, HashMap<i32, i32>>,
+) -> Movement {
   if state.fuel < 0 {
     return state;
   }
@@ -46,14 +54,31 @@ fn inner_count(locations: &Vec<i32>, mut state: Movement) -> Movement {
             end: state.end,
           };
 
-          acc + inner_count(locations, next).paths
+          acc
+            + cache
+              .get(&index)
+              .and_then(|entry| entry.get(&fuel))
+              .map(|ref_count| ref_count.clone())
+              .unwrap_or_else(|| inner_count(locations, next, cache).paths)
         }
       });
+
+  let paths = paths % MOD;
+  match cache.get_mut(&state.current) {
+    Some(other) => other.insert(state.fuel, paths),
+    None => {
+      let mut root = HashMap::new();
+      root.insert(state.fuel, paths);
+      cache.insert(state.current, root);
+      Some(paths)
+    }
+  };
 
   return Movement { paths, ..state };
 }
 
 fn count_routes(locations: Vec<i32>, start: usize, end: usize, fuel: i32) -> i32 {
+  let mut cache = HashMap::new();
   inner_count(
     &locations,
     Movement {
@@ -62,6 +87,7 @@ fn count_routes(locations: Vec<i32>, start: usize, end: usize, fuel: i32) -> i32
       end,
       fuel,
     },
+    &mut cache,
   )
   .paths
 }
@@ -78,5 +104,20 @@ mod tests {
   #[test]
   fn second() {
     assert_eq!(count_routes(vec![4, 3, 1], 1, 0, 6), 5);
+  }
+
+  #[test]
+  fn third() {
+    assert_eq!(count_routes(vec![5, 2, 1], 0, 2, 3), 0);
+  }
+
+  #[test]
+  fn fourth() {
+    assert_eq!(count_routes(vec![2, 1, 5], 0, 0, 3), 2);
+  }
+
+  #[test]
+  fn fifth() {
+    assert_eq!(count_routes(vec![1, 2, 3], 0, 2, 40), 0);
   }
 }
